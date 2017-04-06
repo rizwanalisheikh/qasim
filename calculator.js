4dc9a6303bc2b6d76921c2100c9eb13e15965fa0
MYasirSheikh <m.yasir234@gmail.com>
2017-04-06 16:39:26 +0500

    g

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 angular.module('CalculatorApp', [])
-    .controller('templates.CalculatorController', function($scope) {
+    .controller('CalculatorController', function($scope) {
         $scope.result = function() {
             if ($scope.operator == '+') {
                 return $scope.a + $scope.b;
